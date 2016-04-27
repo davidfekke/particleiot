e7d0157d9024efb6f9d63c9f49699937d0d0b906
davidfekke <david@fekke.com>
2016-04-27 19:24:20 -0400

    Adding environment variables

--- twitterlcd/button.js
+++ twitterlcd/button.js
@@ -2,14 +2,14 @@
 const Particle = require('particle-api-js');
 const particle = new Particle();
 const token = process.env.SPARKACCESSTOKEN;
-const buttonDevice = '2f003b000b47343138333038';
+const buttonDevice = process.env.button_device;
 
 particle.login({
     username: process.env.particle_username, 
     password: process.env.particle_password 
 });
 
-particle.getEventStream({ deviceId: '2f003b000b47343138333038', auth: token }).then(function(stream) {
+particle.getEventStream({ deviceId: buttonDevice, auth: token }).then(function(stream) {
    stream.on('event', function(data) {
      console.log(data);
    });
